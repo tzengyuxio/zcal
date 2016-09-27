dcc85575a0457e232e81a9c6bb773a3559b99c0d
Tzeng Yuxio <tzengyuxio@gmail.com>
2016-09-28 02:58:38 +0800

    Add calculation of Julian/Gregorian calendar to Julian Day Number

--- zcal.go
+++ zcal.go
@@ -3,6 +3,12 @@ package zcal
 var stems = []string{"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}
 var branches = []string{"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}
 
+// JDOfGongheFirstDay 為西曆前 841 年，共和元年立春日
+var JDOfGongheFirstDay = 1414289.5
+
+// JDOfShuodanDongzhi 為西曆 1384年，洪武十七年，朔旦冬至甲子日
+var JDOfShuodanDongzhi = 2226910.5
+
 // StemBranch converts a number into Chinese sexagenary cycle representation.
 func StemBranch(n int) string {
 	g := n % 10
@@ -16,10 +22,57 @@ func StemBranch(n int) string {
 	return stems[g] + branches[z]
 }
 
-// GregorianYearToStemBranch calculates the corresponding stem-branch with the given year.
+// GregorianYearToStemBranch calculates the corresponding stem-branch with the
+// given year.
+//
+// Notice: there is no "0 year" in Gregorian year, the previous year of AD 1 is
+// BC 1 (n == -1). If n == 0, the return value will be the same as n == -1.
 func GregorianYearToStemBranch(n int) string {
 	if n < 0 {
 		return StemBranch((n - 3) % 60)
 	}
 	return StemBranch((n - 4) % 60)
+}
+
+// WesternCalendarToStemBranch calculates the corresponding stem-branch with
+// the given Gregorian year, month and day of month
+func WesternCalendarToStemBranch(y, m, d int) string {
+	var jd float64
+	if y > 1582 || (y == 1582 && m >= 10) {
+		jd = GregorianCalendarToJD(y, m, d)
+	} else {
+		jd = JulianCalendarToJD(y, m, d)
+	}
+	days := int(jd - JDOfShuodanDongzhi)
+	return StemBranch(days)
+}
+
+// GregorianCalendarToJD converts Gregorian calendar date to Julian date.
+func GregorianCalendarToJD(year, month, day int) float64 {
+	a := (14 - month) / 12
+	y := year + 4800 - a
+	m := month + 12*a - 3
+	jdn := day + ((153*m + 2) / 5) + 365*y + (y / 4) - (y / 100) + (y / 400) - 32045
+
+	return float64(jdn) - .5
+}
+
+// JDToGregorianCalendar converts Julian date to Gregorian calendar date.
+func JDToGregorianCalendar(jd float64) (y, m, d int, t float64) {
+	return 0, 0, 0, 0.0
+}
+
+// JulianCalendarToJD converts Julian calendar date to Julian date.
+func JulianCalendarToJD(year, month, day int) float64 {
+	a := (14 - month) / 12
+	y := year + 4800 - a
+	m := month + 12*a - 3
+	jdn := day + ((153*m + 2) / 5) + 365*y + (y / 4) - 32083
+
+	return float64(jdn) - .5
+}
+
+// JDToJulianCalendar converts Julian date to Julian caldendar date.
+func JDToJulianCalendar(jd float64) (y, m, d int, t float64) {
+	return 0, 0, 0, 0.0
 }
--- zcal_test.go
+++ zcal_test.go
@@ -8,22 +8,78 @@ import (
 )
 
 func TestGregorianYearToStemBranch(t *testing.T) {
-	var tests = []struct {
+	for _, pair := range []struct {
 		year       int
 		stemBranch string
 	}{
 		{-2697, "甲子"}, // 黃帝紀元元年
 		{-841, "庚申"},  // 共和元年，中國確切紀年之始
 		{-221, "庚辰"},  // 秦始皇二十六年，統一中國
-		{4, "甲子"},     // 黃帝紀元45甲子(2700年)之後
+		{4, "甲子"},     // 黃帝紀元 45 甲子 (2700 年) 之後
 		{1384, "甲子"},  // 洪武十七年
 		{1894, "甲午"},  // 甲午戰爭
 		{1912, "壬子"},  // 中華民國元年
 		{2012, "壬辰"},
-	}
-
-	for _, pair := range tests {
+	} {
 		stemBranch := GregorianYearToStemBranch(pair.year)
 		assert.Equal(t, pair.stemBranch, stemBranch, "For year %d expected %s got %s", pair.year, pair.stemBranch, stemBranch)
+	}
+}
+
+func TestWesternCalendarToStemBranch(t *testing.T) {
+	for _, pair := range []struct {
+		y, m, d    int
+		stemBranch string
+	}{
+		{1384, 12, 13, "甲子"},
+		{1912, 2, 18, "甲子"},
+		{9912, 2, 18, "甲子"},
+	} {
+		stemBranch := WesternCalendarToStemBranch(pair.y, pair.m, pair.d)
+		assert.Equal(t, pair.stemBranch, stemBranch, "For date %04d-%02d-%02d expected %s got %s", pair.y, pair.m, pair.d, pair.stemBranch, stemBranch)
+	}
+}
+
+func TestGregorianCalendarToJD(t *testing.T) {
+	for _, pair := range []struct {
+		y, m, d int
+		jd      float64
+	}{
+		{-4699, 1, 1, 4785.5},
+		{-2114, 2, 12, 948979.5},
+		{-1050, 3, 23, 1337636.5},
+		{-123, 4, 14, 1676238.5},
+		{-1, 5, 25, 1720838.5},
+		{0, 6, 16, 1721226.5},
+		{1, 7, 27, 1721632.5},
+		{123, 8, 8, 1766203.5},
+		{1678, 9, 9, 2334188.5},
+		{2000, 10, 10, 2451827.5},
+	} {
+		jd := GregorianCalendarToJD(pair.y, pair.m, pair.d)
+		assert.Equal(t, pair.jd, jd, "For date %04d-%02d-%02d expected %.1f got %.1f", pair.y, pair.m, pair.d, pair.jd, jd)
+	}
+}
+
+func TestJulianCalendarToJD(t *testing.T) {
+	for _, pair := range []struct {
+		y, m, d int
+		jd      float64
+	}{
+		{-4712, 1, 2, 0.5},
+		{-4699, 1, 1, 4748.5},
+		{-2114, 2, 12, 948961.5},
+		{-1050, 3, 23, 1337626.5},
+		{-123, 4, 14, 1676235.5},
+		{-1, 5, 25, 1720836.5},
+		{0, 6, 16, 1721224.5},
+		{1, 7, 27, 1721630.5},
+		{123, 8, 8, 1766202.5},
+		{1384, 12, 13, JDOfShuodanDongzhi},
+		{1678, 10, 9, 2334228.5},
+		{2000, 11, 10, 2451871.5},
+	} {
+		jd := JulianCalendarToJD(pair.y, pair.m, pair.d)
+		assert.Equal(t, pair.jd, jd, "For date %04d-%02d-%02d expected %.1f got %.1f", pair.y, pair.m, pair.d, pair.jd, jd)
 	}
 }
